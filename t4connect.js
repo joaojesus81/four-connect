d7189121f7f93a45a5e9b792d18e03c993261a1f
joaojesus81 <joao.jesus@arup.com>
2020-07-25 20:52:23 +0100

    changing focus on terminal 4 connect

---
+++ t4connect.js
@@ -0,0 +1,12 @@
+const matchObject = {
+  A: "| | | | | | |",
+  B: "| | | | | | |",
+  C: "| | | | | | |",
+  D: "| | | | | | |",
+  E: "| | | | | | |",
+  F: "| | | | | | |",
+};
+
+for (const value of Object.values(matchObject)) {
+  console.log(value);
+}
